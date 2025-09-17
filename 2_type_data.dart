dc91bb9cd989bc0b6639f7dea707fac43d674a3d
Aqikol <jaisyahfaith@gmail.com>
2025-09-17 13:57:10 +0700

    Commit ke 2

---
+++ 2_type_data.dart
@@ -0,0 +1,25 @@
+// data yang bisa diubah = mutable
+// data yang tidak bisa diubah = immutable
+/** tipe data immutable ditandai dengan
+ * 
+ * const = compile time constant
+ * final = run time constant
+ * */
+
+const phi = 3.14; // ini adalah imutable
+
+main() {
+  var radius = 7; // mutable
+  radius = 10; // diubah karena mutable
+
+  print("Luas Lingkaran = ${luaslingkaran(radius)}");
+
+  final namaku = "Tio"; // ini immutable
+  final namamu = "Supri"; // ini immutable
+  int umurku = 24; //mutable
+  int umurmu = 100; //mutable
+}
+
+num luaslingkaran(num radius) {
+  return phi * radius * radius;
+}
