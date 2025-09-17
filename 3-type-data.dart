f5b8ce09df0cc81e14aeec21dcd7ecb23ffa6b37
Aqikol <jaisyahfaith@gmail.com>
2025-09-17 14:28:18 +0700

    Belajar const_final dan type_data

---
+++ 3-type-data.dart
@@ -0,0 +1,34 @@
+/**
+ * Variable adalah suatu wadah untuk
+ * menyimpan data
+ */
+
+/**
+ * Tipe data adalah suatu konsep yang digunakan
+ * untuk mengklasifikasi/mengenali jenis
+ * data yang dapat diproses oleh program
+ */
+
+/** Jenis - Jenis Tipe Data
+ * - int = integer = bilangan bulat Contoh nya | 1, -2, 11 dsb
+ * - double = bilangan desimal / pecahan Contoh | 3.14, -0.1, 2.5 dsb
+ * - num = gabungan dari integer dan double Contoh | 3.14, 2, -2, 0, 3.15
+ * - bool = boolean = true or false
+ * - String = string = "Kumpulan Character"
+ * - list = berupa daftar Contoh | [1,2,3,4,5,6,0,-2] & ['a','b','d']
+ * - map = berupa key:value Contoh | {"nama_kota" : "Jakarta", "ID" : 62}
+ * - dynamic = campuran / tipe apapun
+ */
+
+main() {
+  String nama = "Ucup"; //string
+  int age = 24; //integer
+
+  var tinggi_badan; // dynamic
+  tinggi_badan = 159;
+  tinggi_badan = "Seratus enam puluh";
+
+  bool ismaried = false; //boolean
+  double berat_badan = 67.7; // double
+
+}
