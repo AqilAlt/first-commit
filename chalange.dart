6af84c46881a774b92ba140ec5af8a9493b364dc
Aqikol <jaisyahfaith@gmail.com>
2025-09-25 14:18:37 +0700

    Chalange

---
+++ chalange.dart
@@ -0,0 +1,12 @@
+/**
+ * Program menggunakan stdin (input) nilai ujian
+ * car nilaiUjian type data double
+ * ketentuan nilai hasilnya ada di bawah ini
+ * jika nilai kurang dari 0 maka outputnya
+ * "Tidak terdifinisi (Tidak Terdaftar)"
+ * jika nilai lebih dari 100 maka outputnya
+ * "ini adalah cheater"
+ * 
+ * Nilai anda "A/B/C/D/E"
+ * A-c itu lulus dan D-E itu remedial
+ */
