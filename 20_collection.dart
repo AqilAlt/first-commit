1b4a63ad64e7b485aff10c5864ab1fdd1475f811
Aqikol <jaisyahfaith@gmail.com>
2025-10-03 14:16:34 +0700

    Belajar collection & list

---
+++ 20_collection.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  /**
+   * Lsit => berurutan, dibaca dari index 0, bisa sama (duplicate)
+   * Set => Acak, unique
+   * Map => Key Value, dibaca dari key
+   * 
+   * collection adalah struktur data yang digunakan untuk
+   * menyimpan banyak nilai sekaligus
+   * 
+   */
+}
---
+++ 21_list.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+void main(List<String> args) {
+  /**
+   * List menyimpan data secara berurutan dan diakses
+   * melalui index, index dibaca / dimuali
+   * dari index ke 0
+   * 
+   * [ ] => kurung siku / Square bracket
+   * { } => curly bracket
+   * ( ) => Round bracker
+   */
+
+  List<String> buah = ['appel', 'banana', 'cherry', 'durian', 'jack fruit'];
+  print(buah[0]); // output appel
+  // untuk pemanggilan data dari menggunakan [index]
+  // dimuali dari 0
+
+  print('=======ADD========');
+  // menambahkan data ke list diakhir
+  buah.add('water melon');
+  print(buah);
+
+  print('\n=======INSERT=======');
+  // menambahkan element ke list sesuai dengan index
+  buah.insert(3, 'jambu');
+  print(buah);
+
+  print('\n=======REMOVE=======');
+  // menghapus sesuai dengan element (value) yang ada di list
+  buah.remove('banana');
+  print(buah);
+
+  print('\n=====LENGHT=====');
+  // melihat jumlah data / panjang element
+  print(buah);
+  print(buah.length);
+
+  print('\n=========CONSTAINS=========');
+  // mngecek apakah item ada
+  print(buah.contains('water melon'));
+  print(buah.contains('butung'));
+
+  print('\n=======INDEX OF========');
+  // mengecek element di index keberapa di dalam list
+  print(buah.indexOf('water melon'));
+
+  print('\n=======CLEAR========');
+  // menghapus semua element
+  // buah.clear();
+
+  print('\n=======FOREACH========');
+
+  // foreach digunakan untuk memanggil seluruh element dalam list
+  // ((_) => ) lambang (function anonimous)
+  buah.forEach((e) => print(e));
+
+  print('\n');
+
+  // spread list
+
+  List<String> breakfast = [
+    'ayam goyeng',
+    'nasgor',
+    'telor',
+    'sayur bayam',
+    'ayam bakar',
+  ];
+
+  List<String> lunch = [
+    'nasi mandi',
+    'beef',
+    'bakso bakar',
+    'kerang ijo',
+    'es bah',
+  ];
+
+  var allYouCanEat = [breakfast, lunch];
+  print(allYouCanEat);
+
+  print('\n');
+
+  var allYouCanEat2 = [...breakfast, ...lunch];
+  print(allYouCanEat2);
+
+  /**
+   * spread list digunakan untuk
+   * menggabungkan dua list menjadi satu
+   */
+}
